c59021c4d0c3e997361df77e0096c9d36096566c
Matt Borland <matt@mattborland.com>
2023-01-09 13:13:15 -0800

    Add operator== and operator!= to from_chars

--- test/from_chars.cpp
+++ test/from_chars.cpp
@@ -1,4 +1,5 @@
 // Copyright 2022 Peter Dimov
+// Copyright 2023 Matt Borland
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
@@ -8,12 +9,21 @@
 
 int main()
 {
-    char const* buffer = "1048576";
+    const char* buffer = "1048576";
 
     int v = 0;
-    auto r = boost::charconv::from_chars( buffer, buffer + std::strlen( buffer ), v );
+    auto r = boost::charconv::from_chars(buffer, buffer + std::strlen(buffer), v);
 
     BOOST_TEST_EQ( r.ec, 0 ) && BOOST_TEST_EQ( v, 1048576 );
+    BOOST_TEST(r == r);
+
+    boost::charconv::from_chars_result r2 {r.ptr, 0};
+    BOOST_TEST(r == r2);
+
+    const char* buffer2 = "1234567";
+    int v2 = 0;
+    auto r3 = boost::charconv::from_chars(buffer2, buffer2 + std::strlen(buffer), v2);
+    BOOST_TEST(r != r3);
 
     return boost::report_errors();
 }
