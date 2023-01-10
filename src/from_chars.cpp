aae6dd4f4c1f7bca094a82231bd2dd107ba9dea8
Matt Borland <matt@mattborland.com>
2023-01-10 12:01:16 -0800

    Implement integer parser
    [ci skip]

--- src/from_chars.cpp
+++ src/from_chars.cpp
@@ -7,21 +7,109 @@
 #include <boost/charconv/config.hpp>
 #include <boost/config.hpp>
 #include <type_traits>
+#include <limits>
 #include <cstdlib>
+#include <cerrno>
+#include <cstddef>
 
 template <typename Integer, typename std::enable_if<std::is_integral<Integer>::value, bool>::type>
 boost::charconv::from_chars_result boost::charconv::detail::from_chars(const char* first, const char* last, Integer& value, int base) noexcept
 {
+    using Unsinged_Integer = typename std::make_unsigned<Integer>::type;
+    Unsinged_Integer result = 0;
+    Unsinged_Integer overflow_value = 0;
+    Unsinged_Integer max_digit = 0;
+    
     // Check pre-conditions
     BOOST_CHARCONV_ASSERT_MSG(base >= 2 && base <= 36, "Base must be between 2 and 36 (inclusive)");
-
-    BOOST_IF_CONSTEXPR(std::is_same<Integer, int>::value)
+    if (!(first <= last))
     {
-        value = static_cast<int>(std::strtol(first, const_cast<char**>(&last), base));
-        return { last, 0 };
+        return {first, EINVAL};
     }
 
-    return {last, 0};
+    // Strip sign if the type is signed
+    // Negative sign will be appended at the end of parsing
+    bool is_negative = false;
+    auto next = first;
+    BOOST_IF_CONSTEXPR (std::is_signed<Integer>::value)
+    {
+        if (next != last)
+        {
+            if (*next == '-')
+            {
+                is_negative = true;
+                ++next;
+            }
+            else if (*next == '+')
+            {
+                ++next;
+            }
+        }
+
+        if (is_negative)
+        {
+            overflow_value = (static_cast<Unsinged_Integer>((std::numeric_limits<Integer>::max)()) + 1);
+            max_digit = (static_cast<Unsinged_Integer>((std::numeric_limits<Integer>::max)()) + 1);
+        }
+        else
+        {
+            overflow_value = (std::numeric_limits<Integer>::max)();
+            max_digit = (std::numeric_limits<Integer>::max)();
+        }
+    }
+    else
+    {
+        overflow_value = (std::numeric_limits<Unsinged_Integer>::max)();
+        max_digit = (std::numeric_limits<Unsinged_Integer>::max)();
+    }
+
+    overflow_value /= base;
+    max_digit %= base;
+
+    // If the only character was a sign abort now
+    if (next == last)
+    {
+        return {first, EINVAL};
+    }
+
+    bool overflowed = false;
+    while (next != last)
+    {
+        auto current_digit = digit_from_char(*next++);
+
+        if (current_digit >= base)
+        {
+            break;
+        }
+
+        if (result < overflow_value || (result == overflow_value && current_digit <= max_digit))
+        {
+            result = static_cast<Unsinged_Integer>(result * base + current_digit);
+        }
+        else
+        {
+            // Required to keep updating the value of next, but the result is garbage
+            overflowed = true;
+        }
+    }
+
+    // Return the parsed value, adding the sign back if applicable
+    // If we have overflowed then we do not return the result 
+    if (overflowed)
+    {
+        return {next, ERANGE};
+    }
+
+    value = static_cast<Integer>(result);
+    BOOST_IF_CONSTEXPR (std::is_signed<Integer>::value)
+    {
+        if (is_negative)
+        {
+            value *= -1;
+        }
+    }
+
+    return {next, 0};
 }
 
 boost::charconv::from_chars_result boost::charconv::from_chars(const char* first, const char* last, char& value, int base) noexcept
