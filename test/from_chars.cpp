e4c12ebb702f38e66bbc87422ae10cbb13b0ca25
Matt Borland <matt@mattborland.com>
2023-01-11 07:31:48 -0800

    Refactor simple test

--- test/from_chars.cpp
+++ test/from_chars.cpp
@@ -6,24 +6,44 @@
 #include <boost/charconv.hpp>
 #include <boost/core/lightweight_test.hpp>
 #include <cstring>
+#include <cstdint>
 
-int main()
+// No overflows, negative numbers, locales, etc.
+template <typename T>
+void simple_test()
 {
-    const char* buffer = "1048576";
+    const char* buffer = "34";
 
-    int v = 0;
+    T v = 0;
     auto r = boost::charconv::from_chars(buffer, buffer + std::strlen(buffer), v);
 
-    BOOST_TEST_EQ( r.ec, 0 ) && BOOST_TEST_EQ( v, 1048576 );
+    BOOST_TEST_EQ( r.ec, 0 ) && BOOST_TEST_EQ(v, 34);
     BOOST_TEST(r == r);
 
     boost::charconv::from_chars_result r2 {r.ptr, 0};
     BOOST_TEST(r == r2);
 
-    const char* buffer2 = "1234567";
-    int v2 = 0;
+    const char* buffer2 = "12";
+    T v2 = 0;
     auto r3 = boost::charconv::from_chars(buffer2, buffer2 + std::strlen(buffer), v2);
     BOOST_TEST(r != r3);
+    BOOST_TEST_EQ(r3.ec, 0) && BOOST_TEST_EQ(v2, 12);
+}
 
+int main()
+{
+    simple_test<char>();
+    simple_test<unsigned char>();
+    simple_test<short>();
+    simple_test<unsigned short>();
+    simple_test<int>();
+    simple_test<unsigned>();
+    simple_test<long>();
+    simple_test<unsigned long>();
+    simple_test<long long>();
+    simple_test<unsigned long long>();
+    simple_test<std::int32_t>();
+    simple_test<std::uint64_t>();
+    
     return boost::report_errors();
 }
