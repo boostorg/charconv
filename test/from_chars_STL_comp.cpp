2c33b21dc22e61d67a001b7148a0e34746d61027
Matt Borland <matt@mattborland.com>
2023-01-13 11:43:06 -0800

    Add more edge cases to comparison test

--- test/from_chars_STL_comp.cpp
+++ test/from_chars_STL_comp.cpp
@@ -15,7 +15,7 @@
 template <typename T>
 void test()
 {
-    // Base 2
+    // Binary
     const char* base2 = "0101010";
     T base2_v_boost = 0;
     T base2_v_stl = 0;
@@ -32,6 +32,39 @@ void test()
     auto r2_stl = std::from_chars(base16, base16 + std::strlen(base16), base16_v_stl, 16);
     BOOST_TEST_EQ(r2_boost.ptr, r2_stl.ptr);
     BOOST_TEST_EQ(base16_v_boost, base16_v_stl);
+
+    base16 = "2a";
+    r2_boost = boost::charconv::from_chars(base16, base16 + std::strlen(base16), base16_v_boost, 16);
+    r2_stl = std::from_chars(base16, base16 + std::strlen(base16), base16_v_stl, 16);
+    BOOST_TEST_EQ(r2_boost.ptr, r2_stl.ptr);
+    BOOST_TEST_EQ(base16_v_boost, base16_v_stl);
+
+    // Decimal
+    const char* base10 = "42";
+    T base10_v_boost = 0;
+    T base10_v_stl = 0;
+    auto r3_boost = boost::charconv::from_chars(base10, base10 + std::strlen(base10), base10_v_boost);
+    auto r3_stl = std::from_chars(base10, base10 + std::strlen(base10), base10_v_stl);
+    BOOST_TEST_EQ(r3_boost.ptr, r3_stl.ptr);
+    BOOST_TEST_EQ(base10_v_boost, base10_v_stl);
+
+    // Negative number
+    const char* neg = "-100";
+    T negative_v_boost = 0;
+    T negative_v_stl = 0;
+    auto r4_boost = boost::charconv::from_chars(neg, neg + std::strlen(neg), negative_v_boost);
+    auto r4_stl = std::from_chars(neg, neg + std::strlen(neg), negative_v_stl);
+    BOOST_TEST_EQ(r4_boost.ptr, r4_stl.ptr);
+    BOOST_TEST_EQ(negative_v_boost, negative_v_stl);
+
+    // Digit seperator
+    const char* sep = "1'000";
+    T sep_v_boost = 0;
+    T sep_v_stl = 0;
+    auto r5_boost = boost::charconv::from_chars(sep, sep + std::strlen(sep), sep_v_boost);
+    auto r5_stl = std::from_chars(sep, sep + std::strlen(sep), sep_v_stl);
+    BOOST_TEST_EQ(r5_boost.ptr, r5_stl.ptr);
+    BOOST_TEST_EQ(sep_v_boost, sep_v_stl);
 }
 
 int main()
