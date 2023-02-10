b46dc2988b761c35650e37564b2e82efabedce39
Peter Dimov <pdimov@gmail.com>
2023-02-10 21:51:27 +0200

    Add limits<T> specializations for (u)int128, because std::numeric_limits isn't always correct

--- include/boost/charconv/limits.hpp
+++ include/boost/charconv/limits.hpp
@@ -39,6 +39,24 @@ template<typename T> struct limits
         std::numeric_limits<T>::max_digits10 + 3 + 2 + detail::exp_digits( std::numeric_limits<T>::max_exponent10 ); // as above
 };
 
+#if defined(BOOST_HAS_INT128)
+
+// std::numeric_limits is not always specialized for __int128_t
+
+template<> struct limits<boost::int128_type>
+{
+    static constexpr int max_chars10 = 38 + 2; // digits10 + 1 + sign
+    static constexpr int max_chars = 127 + 2; // digits + 1 + sign
+};
+
+template<> struct limits<boost::uint128_type>
+{
+    static constexpr int max_chars10 = 38 + 1; // digits10 + 1
+    static constexpr int max_chars = 128 + 1; // digits + 1
+};
+
+#endif // #if defined(BOOST_HAS_INT128)
+
 #if defined(BOOST_NO_CXX17_INLINE_VARIABLES)
 
 // Definitions of in-class constexpr members are allowed but deprecated in C++17
