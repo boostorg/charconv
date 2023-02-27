289b17c2930065c8b4df4b626f5204682ed2d94e
Matt Borland <matt@mattborland.com>
2023-02-27 10:30:53 -0800

    Add trivial functioning long double impl

--- include/boost/charconv/detail/compute_float80.hpp
+++ include/boost/charconv/detail/compute_float80.hpp
@@ -6,49 +6,23 @@
 #define BOOST_CHARCONV_DETAIL_COMPUTE_FLOAT80_HPP
 
 #include <boost/charconv/detail/config.hpp>
-#include <boost/charconv/detail/bit_layouts.hpp>
-#include <boost/charconv/to_chars.hpp>
-#include <string>
-#include <bitset>
 #include <cstdint>
-#include <cstring>
 #include <cmath>
 
 namespace boost { namespace charconv { namespace detail {
 
+// TODO: Trivial implementation
 inline long double compute_float80(std::int64_t power, std::uint64_t i, bool negative, bool& success) noexcept
 {
-    static constexpr auto e_bias = 16383; // See IEEE 754-2019 Section 3.6 binary128
+    long double return_val = i * std::pow(10.0L, power);
 
-    if (power > e_bias || power < 2 - e_bias)
+    if (return_val == HUGE_VALL)
     {
         success = false;
-        return negative ? -0.0L : 0.0L; 
+        return negative ? -0.0L : 0.0L;
     }
 
-    char buffer[80];
-    if (negative)
-    {
-        buffer[0] = static_cast<char>(48);
-    }
-    else
-    {
-        buffer[0] = static_cast<char>(49);
-    }
-
-    char power_buffer[15];
-    to_chars(power_buffer, power_buffer + sizeof(power_buffer), e_bias - power, 2);
-    std::memcpy(buffer + 1, power_buffer, sizeof(power_buffer));
-
-    char significand_buffer[64];
-    to_chars(significand_buffer, significand_buffer + sizeof(significand_buffer), i, 2);
-    std::memcpy(buffer + 16, significand_buffer, sizeof(significand_buffer));
-
-
-    std::bitset<80> bit_val {buffer};
-    long double return_val;
-
-    std::memcpy(&return_val, &bit_val, sizeof(return_val));
+    return_val = negative ? -return_val : return_val;
 
     success = true;
     return return_val;
