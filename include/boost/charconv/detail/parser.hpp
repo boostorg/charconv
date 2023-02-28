1688710f5276aeb8ef2bd8763844483c481ed2e0
Matt Borland <matt@mattborland.com>
2023-02-28 10:00:07 -0800

    Fix parsing of scientific notation hex values

--- include/boost/charconv/detail/parser.hpp
+++ include/boost/charconv/detail/parser.hpp
@@ -8,6 +8,7 @@
 #include <boost/charconv/detail/config.hpp>
 #include <boost/charconv/detail/from_chars_result.hpp>
 #include <boost/charconv/detail/from_chars_integer_impl.hpp>
+#include <boost/charconv/detail/integer_search_trees.hpp>
 #include <boost/charconv/chars_format.hpp>
 #include <limits>
 #include <cerrno>
@@ -174,7 +175,7 @@ inline from_chars_result parser_impl(const char* first, const char* last, bool& 
 
     // Finally we get the exponent
     char exponent_buffer[11] {}; // Binary64 maximum from IEEE 754-2019 section 3.6
-    Integer significand_digits = i - 1;
+    Integer significand_digits = i;
     i = 0;
     while (next != last)
     {
@@ -193,7 +194,17 @@ inline from_chars_result parser_impl(const char* first, const char* last, bool& 
         default:
             if (fractional)
             {
-                exponent -= significand_digits;
+                // Need to take the offset from 1.xxx becuase compute_floatXXX assumes the significand is an integer
+                // so the exponent is off by the number of digits in the significand - 1
+                if (fmt == chars_format::hex)
+                {
+                    // In hex the number of digits parsed is possibly less than the number of digits in base10
+                    exponent -= num_digits(significand) - 1;
+                }
+                else
+                {
+                    exponent -= significand_digits - 1;
+                }
             }
             return {next, 0};
     }
--- test/from_chars_float.cpp
+++ test/from_chars_float.cpp
@@ -59,6 +59,22 @@ void simple_scientific_test()
     BOOST_TEST_EQ(v3, static_cast<T>(1.23456789e+10L));
 }
 
+template <typename T>
+void simple_hex_scientifc_test()
+{
+    const char* buffer1 = "1.3a2bp-10";
+    T v1 = 0;
+    auto r1 = boost::charconv::from_chars(buffer1, buffer1 + std::strlen(buffer1), v1, boost::charconv::chars_format::hex);
+    BOOST_TEST_EQ(r1.ec, 0);
+    BOOST_TEST_EQ(v1, static_cast<T>(80427e-14L));
+
+    const char* buffer2 = "1.234p-10";
+    T v2 = 0;
+    auto r2 = boost::charconv::from_chars(buffer2, buffer2 + std::strlen(buffer2), v2, boost::charconv::chars_format::hex);
+    BOOST_TEST_EQ(r2.ec, 0);
+    BOOST_TEST_EQ(v2, static_cast<T>(4660e-13L));
+}
+
 int main()
 {
     simple_integer_test<float>();
@@ -69,6 +85,9 @@ int main()
 
     simple_scientific_test<float>();
     simple_scientific_test<double>();
+
+    simple_hex_scientifc_test<float>();
+    simple_hex_scientifc_test<double>();
 
     return boost::report_errors();
 }
