27f6cc76f529089f0fe303d47c68faccb92c0b32
Matt Borland <matt@mattborland.com>
2023-03-09 10:33:45 -0800

    Remove unneeded paths

--- include/boost/charconv/detail/compute_float80.hpp
+++ include/boost/charconv/detail/compute_float80.hpp
@@ -12,39 +12,15 @@
 
 namespace boost { namespace charconv { namespace detail {
 
-static constexpr long double big_powers[] = {
-    1e4892L, 1e4893L, 1e4894L, 1e4895L, 1e4896L, 1e4897L, 1e4898L,
-    1e4899L, 1e4900L, 1e4901L, 1e4902L, 1e4903L, 1e4904L, 1e4905L,
-    1e4906L, 1e4907L, 1e4908L, 1e4909L, 1e4910L, 1e4911L, 1e4912L
-};
-
-static_assert(sizeof(big_powers) == 21*sizeof(long double), "Big powers table must have 21 elements");
-
 inline long double compute_float80(std::int64_t power, boost::uint128_type i, bool negative, bool& success) noexcept
 {
     long double return_val;
 
-    // At the absolute minimum and maximum rounding errors of 1 ULP can cause overflow
-    if (power == 4912 && static_cast<std::uint64_t>(i) == UINT64_C(8292685093465866806))
+    return_val = static_cast<long double>(i * std::pow(10.0L, static_cast<long double>(power)));
+    if (std::abs(return_val) > (std::numeric_limits<long double>::max)())
     {
-        return_val = std::numeric_limits<long double>::max();
-    }
-    else if (power == -4952 && static_cast<std::uint64_t>(i) == UINT64_C(4168920984437421538))
-    {
-        return_val = std::numeric_limits<long double>::min();
-    }
-    else if (power >= 4892)
-    {
-        return_val = i * big_powers[power - 4892];
-    }
-    else
-    {
-        return_val = static_cast<long double>(i * std::pow(10.0L, static_cast<long double>(power)));
-        if (std::abs(return_val) > (std::numeric_limits<long double>::max)())
-        {
-            success = false;
-            return negative ? -0.0L : 0.0L;
-        }
+        success = false;
+        return negative ? -0.0L : 0.0L;
     }
 
     return_val = negative ? -return_val : return_val;
