744531fc0ac764b6f889b5f5ad4ddf0eb8418406
Matt Borland <matt@mattborland.com>
2023-04-19 16:37:49 +0200

    Add test for non-finite values

--- test/to_chars_float.cpp
+++ test/to_chars_float.cpp
@@ -49,10 +49,33 @@ void integer_general_format()
     BOOST_TEST_EQ(return_v1, v1);
 }
 
+template <typename T>
+void non_finite_values()
+{
+    char buffer1[256] {};
+    T v1 = std::numeric_limits<T>::infinity();
+    auto r1 = boost::charconv::to_chars(buffer1, buffer1 + sizeof(buffer1), v1);
+    BOOST_TEST_EQ(r1.ec, 0);
+    BOOST_TEST_CSTR_EQ(buffer1, "Infinity");
+
+    char buffer2[256] {};
+    T v2 = -std::numeric_limits<T>::infinity();
+    auto r2 = boost::charconv::to_chars(buffer2, buffer2 + sizeof(buffer2), v2);
+    BOOST_TEST_EQ(r2.ec, 0);
+    BOOST_TEST_CSTR_EQ(buffer2, "-Infinity");
+
+    char buffer3[256] {};
+    T v3 = std::numeric_limits<T>::quiet_NaN();
+    auto r3 = boost::charconv::to_chars(buffer3, buffer3 + sizeof(buffer3), v3);
+    BOOST_TEST_EQ(r3.ec, 0);
+    BOOST_TEST_CSTR_EQ(buffer3, "NaN");
+}
+
 int main()
 {
     printf_divergence<double>();
     integer_general_format<double>();
+    non_finite_values<double>();
 
     return boost::report_errors();
 }
