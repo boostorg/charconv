60c16b776744bb80511a0477fc0a3f63359d6d37
Matt Borland <matt@mattborland.com>
2023-05-15 12:38:57 +0200

    Fix 64-bit and MSVC long double handling

--- src/from_chars.cpp
+++ src/from_chars.cpp
@@ -87,9 +87,13 @@ boost::charconv::from_chars_result boost::charconv::from_chars(const char* first
 // Since long double is just a double we use the double implementation and cast into value
 boost::charconv::from_chars_result boost::charconv::from_chars(const char* first, const char* last, long double& value, boost::charconv::chars_format fmt) noexcept
 {
-    double d;
-    auto r = boost::charconv::from_chars(first, last, d, fmt);
-    value = static_cast<long double>(d);
+    double d {};
+    const auto r = boost::charconv::from_chars(first, last, d, fmt);
+    if (r.ec == 0)
+    {
+        value = static_cast<long double>(d);
+    }
+
     return r;
 }
 
