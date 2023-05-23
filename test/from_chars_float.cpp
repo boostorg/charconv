0b0d0b671881d138ce74a94487b8c18b50febde8
Matt Borland <matt@mattborland.com>
2023-05-23 17:32:47 +0200

    Add test value

--- test/from_chars_float.cpp
+++ test/from_chars_float.cpp
@@ -449,6 +449,24 @@ void test_strtod_routines(T val, const char* str)
     }
 }
 
+// Parser ignoring null terminator
+template <typename T>
+void test_issue_48(const T val, const char* str, const std::ptrdiff_t expected_pos, boost::charconv::chars_format fmt = boost::charconv::chars_format::scientific)
+{
+    const char* last = str + std::strlen(str);
+    T from_val;
+    const auto r = boost::charconv::from_chars(str, last, from_val, fmt);
+
+    if (!(BOOST_TEST_EQ(from_val, val) && BOOST_TEST(r.ec == std::errc())))
+    {
+        std::cerr << std::setprecision(std::numeric_limits<T>::digits10 + 1)
+                  << "\nFrom chars value: " << from_val
+                  << "\n      Original V: " << val
+                  << "\n  From chars ptr: " << (r.ptr - str)
+                  << "\n    Expected ptr: " << expected_pos << std::endl;
+    }
+}
+
 int main()
 {
     simple_integer_test<float>();
@@ -492,6 +510,8 @@ int main()
     test_issue_45<double>(static_cast<double>(-1985444280612224.5), "-1985444280612224.5e+258", 19);
     test_issue_45<double>(2.196197480766336e-308, "2.196197480766336e-30889", 22);
     test_issue_45<double>(static_cast<double>(278061055647717.5), "278061055647717.5e-2288", 17);
+
+    test_issue_48(-1.132185940257003e+207, "-1.132185940257003e+207\05.406741134053704e+77\0", 23);
 
     // Value in range with 20 million digits. Malloc should max out at 16'711'568 bytes
     test_strtod_routines<double>(1.982645139827653964857196,
