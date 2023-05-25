86f30a37c30cced102299926fbe0256502d1a2d2
Peter Dimov <pdimov@gmail.com>
2023-05-25 22:12:22 +0300

    Add precision=6 cases to to_chars_floating

--- benchmark/to_chars_floating.cpp
+++ benchmark/to_chars_floating.cpp
@@ -37,17 +37,24 @@ template<class T> static BOOST_NOINLINE void init_input_data( std::vector<T>& da
 
 using namespace std::chrono_literals;
 
-template<class T> static BOOST_NOINLINE void test_snprintf( std::vector<T> const& data, bool general, char const* label )
+template<class T> static BOOST_NOINLINE void test_snprintf( std::vector<T> const& data, bool general, char const* label, int precision )
 {
     auto t1 = std::chrono::steady_clock::now();
 
     std::size_t s = 0;
 
-    char const* format = general? "%.16g": "%.16e";
+    char const* format = general? "%.*g": "%.*e";
 
-    BOOST_IF_CONSTEXPR( std::is_same<T, float>::value )
+    int prec = precision;
+
+    if( prec == 0 )
     {
-        format = general? "%.8g": "%.8e";
+        prec = 16;
+
+        BOOST_IF_CONSTEXPR( std::is_same<T, float>::value )
+        {
+            prec = 8;
+        }
     }
 
     for( int i = 0; i < K; ++i )
@@ -56,7 +63,7 @@ template<class T> static BOOST_NOINLINE void test_snprintf( std::vector<T> const
 
         for( auto x: data )
         {
-            auto r = std::snprintf( buffer, sizeof( buffer ), format, x );
+            auto r = std::snprintf( buffer, sizeof( buffer ), format, prec, x );
             s += r;
             s += static_cast<unsigned char>( buffer[0] );
         }
@@ -64,10 +71,10 @@ template<class T> static BOOST_NOINLINE void test_snprintf( std::vector<T> const
 
     auto t2 = std::chrono::steady_clock::now();
 
-    std::cout << "            std::snprintf<" << boost::core::type_name<T>() << ">, " << label << ": " << std::setw( 5 ) << ( t2 - t1 ) / 1ms << " ms (s=" << s << ")\n";
+    std::cout << "            std::snprintf<" << boost::core::type_name<T>() << ">, " << label << ", " << precision << ": " << std::setw( 5 ) << ( t2 - t1 ) / 1ms << " ms (s=" << s << ")\n";
 }
 
-template<class T> static BOOST_NOINLINE void test_std_to_chars( std::vector<T> const& data, bool general, char const* label )
+template<class T> static BOOST_NOINLINE void test_std_to_chars( std::vector<T> const& data, bool general, char const* label, int precision )
 {
     auto t1 = std::chrono::steady_clock::now();
 
@@ -79,7 +86,12 @@ template<class T> static BOOST_NOINLINE void test_std_to_chars( std::vector<T> c
 
         for( auto x: data )
         {
-            auto r = std::to_chars( buffer, buffer + sizeof( buffer ), x, general? std::chars_format::general: std::chars_format::scientific );
+            std::chars_format fmt = general? std::chars_format::general: std::chars_format::scientific;
+
+            auto r = precision == 0?
+                std::to_chars( buffer, buffer + sizeof( buffer ), x, fmt ):
+                std::to_chars( buffer, buffer + sizeof( buffer ), x, fmt, precision );
+
             s += static_cast<std::size_t>( r.ptr - buffer );
             s += static_cast<unsigned char>( buffer[0] );
         }
@@ -87,10 +99,10 @@ template<class T> static BOOST_NOINLINE void test_std_to_chars( std::vector<T> c
 
     auto t2 = std::chrono::steady_clock::now();
 
-    std::cout << "            std::to_chars<" << boost::core::type_name<T>() << ">, " << label << ": " << std::setw( 5 ) << ( t2 - t1 ) / 1ms << " ms (s=" << s << ")\n";
+    std::cout << "            std::to_chars<" << boost::core::type_name<T>() << ">, " << label << ", " << precision << ": " << std::setw( 5 ) << ( t2 - t1 ) / 1ms << " ms (s=" << s << ")\n";
 }
 
-template<class T> static BOOST_NOINLINE void test_boost_to_chars( std::vector<T> const& data, bool general, char const* label )
+template<class T> static BOOST_NOINLINE void test_boost_to_chars( std::vector<T> const& data, bool general, char const* label, int precision )
 {
     auto t1 = std::chrono::steady_clock::now();
 
@@ -102,7 +114,12 @@ template<class T> static BOOST_NOINLINE void test_boost_to_chars( std::vector<T>
 
         for( auto x: data )
         {
-            auto r = boost::charconv::to_chars( buffer, buffer + sizeof( buffer ), x, general? boost::charconv::chars_format::general: boost::charconv::chars_format::scientific );
+            boost::charconv::chars_format fmt = general? boost::charconv::chars_format::general: boost::charconv::chars_format::scientific;
+
+            auto r = precision == 0?
+                boost::charconv::to_chars( buffer, buffer + sizeof( buffer ), x, fmt ):
+                boost::charconv::to_chars( buffer, buffer + sizeof( buffer ), x, fmt, precision );
+
             s += static_cast<std::size_t>( r.ptr - buffer );
             s += static_cast<unsigned char>( buffer[0] );
         }
@@ -110,7 +127,7 @@ template<class T> static BOOST_NOINLINE void test_boost_to_chars( std::vector<T>
 
     auto t2 = std::chrono::steady_clock::now();
 
-    std::cout << "boost::charconv::to_chars<" << boost::core::type_name<T>() << ">, " << label << ": " << std::setw( 5 ) << ( t2 - t1 ) / 1ms << " ms (s=" << s << ")\n";
+    std::cout << "boost::charconv::to_chars<" << boost::core::type_name<T>() << ">, " << label << ", " << precision << ": " << std::setw( 5 ) << ( t2 - t1 ) / 1ms << " ms (s=" << s << ")\n";
 }
 
 template<class T> static void test()
@@ -118,15 +135,27 @@ template<class T> static void test()
     std::vector<T> data;
     init_input_data( data );
 
-    test_snprintf( data, false, "scientific" );
-    test_std_to_chars( data, false, "scientific" );
-    test_boost_to_chars( data, false, "scientific" );
+    test_snprintf( data, false, "scientific", 0 );
+    test_std_to_chars( data, false, "scientific", 0 );
+    test_boost_to_chars( data, false, "scientific", 0 );
 
     std::cout << std::endl;
 
-    test_snprintf( data, true, "general" );
-    test_std_to_chars( data, true, "general" );
-    test_boost_to_chars( data, true, "general" );
+    test_snprintf( data, false, "scientific", 6 );
+    test_std_to_chars( data, false, "scientific", 6 );
+    test_boost_to_chars( data, false, "scientific", 6 );
+
+    std::cout << std::endl;
+
+    test_snprintf( data, true, "general", 0 );
+    test_std_to_chars( data, true, "general", 0 );
+    test_boost_to_chars( data, true, "general", 0 );
+
+    std::cout << std::endl;
+
+    test_snprintf( data, true, "general", 6 );
+    test_std_to_chars( data, true, "general", 6 );
+    test_boost_to_chars( data, true, "general", 6 );
 
     std::cout << std::endl;
 }
