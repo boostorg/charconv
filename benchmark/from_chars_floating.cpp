e724e69a859f73909781135b5feac423f7e4501e
Peter Dimov <pdimov@gmail.com>
2023-05-25 20:08:25 +0300

    Add benchmark/from_chars_floating.cpp

---
+++ benchmark/from_chars_floating.cpp
@@ -0,0 +1,188 @@
+// Copyright 2023 Peter Dimov
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#include <boost/charconv/from_chars.hpp>
+#include <boost/charconv/to_chars.hpp>
+#include <boost/core/type_name.hpp>
+#include <boost/core/detail/splitmix64.hpp>
+#include <boost/config.hpp>
+#include <type_traits>
+#include <chrono>
+#include <cstdio>
+#include <iostream>
+#include <iomanip>
+#include <charconv>
+
+constexpr unsigned N = 2'000'000;
+constexpr int K = 10;
+
+template<class T> static void init_input_data( std::vector<std::string>& data, bool general )
+{
+    data.reserve( N );
+
+    boost::detail::splitmix64 rng;
+
+    for( unsigned i = 0; i < N; ++i )
+    {
+        std::uint64_t tmp = rng();
+
+        T x;
+        std::memcpy( &x, &tmp, sizeof(x) );
+
+        if( !std::isfinite(x) ) continue;
+
+        char buffer[ 64 ];
+        auto r = boost::charconv::to_chars( buffer, buffer + sizeof( buffer ), x, general? boost::charconv::chars_format::general: boost::charconv::chars_format::scientific );
+
+        std::string y( buffer, r.ptr );
+        data.push_back( y );
+    }
+}
+
+template<class T> static void init_input_data_uint64( std::vector<std::string>& data )
+{
+    data.reserve( N );
+
+    boost::detail::splitmix64 rng;
+
+    for( unsigned i = 0; i < N; ++i )
+    {
+        std::uint64_t x = rng();
+
+        char buffer[ 64 ];
+        auto r = boost::charconv::to_chars( buffer, buffer + sizeof( buffer ), x );
+
+        std::string y( buffer, r.ptr );
+        data.push_back( y );
+    }
+}
+
+using namespace std::chrono_literals;
+
+template<class T> void test_strtox( std::vector<std::string> const& data, bool general, char const* label );
+
+template<> void test_strtox<float>( std::vector<std::string> const& data, bool, char const* label )
+{
+    auto t1 = std::chrono::steady_clock::now();
+
+    double s = 0;
+
+    for( int i = 0; i < K; ++i )
+    {
+        for( auto const& x: data )
+        {
+            float y = std::strtof( x.c_str(), nullptr );
+            s = s / 16.0 + y;
+        }
+    }
+
+    auto t2 = std::chrono::steady_clock::now();
+
+    std::cout << "                std::strtox<float>, " << label << ": " << std::setw( 5 ) << ( t2 - t1 ) / 1ms << " ms (s=" << s << ")\n";
+}
+
+template<> void test_strtox<double>( std::vector<std::string> const& data, bool, char const* label )
+{
+    auto t1 = std::chrono::steady_clock::now();
+
+    double s = 0;
+
+    for( int i = 0; i < K; ++i )
+    {
+        for( auto const& x: data )
+        {
+            double y = std::strtod( x.c_str(), nullptr );
+            s = s / 16.0 + y;
+        }
+    }
+
+    auto t2 = std::chrono::steady_clock::now();
+
+    std::cout << "                std::strtox<double>, " << label << ": " << std::setw( 5 ) << ( t2 - t1 ) / 1ms << " ms (s=" << s << ")\n";
+}
+
+template<class T> static void test_std_from_chars( std::vector<std::string> const& data, bool general, char const* label )
+{
+    auto t1 = std::chrono::steady_clock::now();
+
+    double s = 0;
+
+    for( int i = 0; i < K; ++i )
+    {
+        for( auto const& x: data )
+        {
+            T y;
+            std::from_chars( x.data(), x.data() + x.size(), y, general? std::chars_format::general: std::chars_format::scientific );
+
+            s = s / 16.0 + y;
+        }
+    }
+
+    auto t2 = std::chrono::steady_clock::now();
+
+    std::cout << "            std::from_chars<" << boost::core::type_name<T>() << ">, " << label << ": " << std::setw( 5 ) << ( t2 - t1 ) / 1ms << " ms (s=" << s << ")\n";
+}
+
+template<class T> static void test_boost_from_chars( std::vector<std::string> const& data, bool general, char const* label )
+{
+    auto t1 = std::chrono::steady_clock::now();
+
+    double s = 0;
+
+    for( int i = 0; i < K; ++i )
+    {
+        for( auto const& x: data )
+        {
+            T y;
+            boost::charconv::from_chars( x.data(), x.data() + x.size(), y, general? boost::charconv::chars_format::general: boost::charconv::chars_format::scientific );
+
+            s = s / 16.0 + y;
+        }
+    }
+
+    auto t2 = std::chrono::steady_clock::now();
+
+    std::cout << "boost::charconv::from_chars<" << boost::core::type_name<T>() << ">, " << label << ": " << std::setw( 5 ) << ( t2 - t1 ) / 1ms << " ms (s=" << s << ")\n";
+}
+
+template<class T> static void test( bool general )
+{
+    std::vector<std::string> data;
+    init_input_data<T>( data, general );
+
+    char const* label = general? "general": "scientific";
+
+    test_strtox<T>( data, general, label );
+    test_std_from_chars<T>( data, general, label );
+    test_boost_from_chars<T>( data, general, label );
+
+    std::cout << std::endl;
+}
+
+template<class T> static void test2()
+{
+    std::vector<std::string> data;
+    init_input_data_uint64<T>( data );
+
+    bool general = true;
+    char const* label = "uint64";
+
+    test_strtox<T>( data, general, label );
+    test_std_from_chars<T>( data, general, label );
+    test_boost_from_chars<T>( data, general, label );
+
+    std::cout << std::endl;
+}
+
+int main()
+{
+    test<float>( false );
+    test<double>( false );
+
+    test<float>( true );
+    test<double>( true );
+
+    test2<float>();
+    test2<double>();
+}
