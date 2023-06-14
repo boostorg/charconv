9081ece4481bfcad170358332bd9d0ad892e2e90
Matt Borland <matt@mattborland.com>
2023-06-14 13:00:18 +0200

    Improve printing of single character

--- src/to_chars.cpp
+++ src/to_chars.cpp
@@ -68,14 +68,7 @@ namespace boost { namespace charconv { namespace detail { namespace to_chars_det
 
     static void print_1_digit(std::uint32_t n, char* buffer) noexcept
     {
-        BOOST_IF_CONSTEXPR (('0' & 0xf) == 0)
-        {
-            *buffer = char('0' | n);
-        }
-        else
-        {
-            *buffer = char('0' + n);
-        }
+        *buffer = char('0' | n);
     }
 
     static void print_2_digits(std::uint32_t n, char* buffer) noexcept 
