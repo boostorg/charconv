ded399f0b1ef6255957aad760d5e38edc2dba2a1
Matt Borland <matt@mattborland.com>
2023-06-14 11:32:56 +0200

    Add 128-bit sprintf testing

--- test/test_float128.cpp
+++ test/test_float128.cpp
@@ -45,6 +45,7 @@ std::ostream& operator<<( std::ostream& os, __float128 v )
 #include <limits>
 #include <iostream>
 #include <iomanip>
+#include <string>
 
 constexpr int N = 1024;
 static boost::detail::splitmix64 rng;
@@ -97,6 +98,139 @@ void test_roundtrip( T value )
     }
 }
 
+const char* fmt_from_type (__float128)
+{
+    return "%Qg";
+}
+
+const char* fmt_from_type_fixed (__float128)
+{
+    return "%.0Qf";
+}
+
+const char* fmt_from_type_scientific (__float128)
+{
+    return "%.35Qe";
+}
+
+const char* fmt_from_type_hex (__float128)
+{
+    return "%Qa";
+}
+
+inline int print(__float128 value, char* buffer, size_t buffer_size, const char* fmt)
+{
+    return quadmath_snprintf(buffer, buffer_size, fmt, value);
+}
+
+#ifdef BOOST_CHARCONV_HAS_STDFLOAT128
+const char* fmt_from_type (std::float128_t)
+{
+    return "%F128g";
+}
+
+const char* fmt_from_type_fixed (std::float128_t)
+{
+    return "%.0F128f";
+}
+
+const char* fmt_from_type_scientific (std::float128_t)
+{
+    return "%.35F128e";
+}
+
+const char* fmt_from_type_hex (std::float128_t)
+{
+    return "%F128a";
+}
+
+inline int print(std::float128_t value, char* buffer, size_t buffer_size, const char* fmt)
+{
+    return std::snprintf(buffer, buffer_size, fmt, value);
+}
+#endif
+
+template <typename T>
+void test_sprintf_float( T value, boost::charconv::chars_format fmt = boost::charconv::chars_format::scientific )
+{
+    char buffer [ 256 ] {};
+
+    const auto r = boost::charconv::to_chars( buffer, buffer + sizeof(buffer), value, fmt );
+    BOOST_TEST( r.ec == std::errc() );
+
+    char buffer2 [ 256 ] {};
+
+    const char* sprintf_fmt;
+    const char* error_format;
+    switch (fmt)
+    {
+        case boost::charconv::chars_format::general:
+            sprintf_fmt = fmt_from_type(value);
+            error_format = "General";
+            break;
+        case boost::charconv::chars_format::scientific:
+            sprintf_fmt = fmt_from_type_scientific(value);
+            error_format = "Scientific";
+            break;
+        case boost::charconv::chars_format::fixed:
+            sprintf_fmt = fmt_from_type_fixed(value);
+            error_format = "Fixed";
+            break;
+        case boost::charconv::chars_format::hex:
+            sprintf_fmt = fmt_from_type_hex(value);
+            error_format = "Hex";
+            break;
+    }
+
+    print( value, buffer2, sizeof(buffer2), sprintf_fmt );
+
+    // Remove trailing zeros from printf (if applicable)
+    std::string printf_string {buffer2};
+
+    #ifndef __i686__
+    if (fmt == boost::charconv::chars_format::scientific)
+    {
+        std::size_t found_trailing_0 = printf_string.find_first_of('e');
+        if (found_trailing_0 != std::string::npos)
+        {
+            --found_trailing_0;
+            while (printf_string[found_trailing_0] == '0')
+            {
+                printf_string.erase(found_trailing_0, 1);
+                --found_trailing_0;
+            }
+        }
+    }
+    #endif
+
+    // Same issues that arise in to_chars_snprintf.cpp so abort if in range
+    //
+    //     Value: 3.350549627872214798203501062446534e-4913
+    //  To chars: 3.350549627872214798203501062446534e-4913
+    //  Snprintf: 3.35055e-4913
+    //
+    //     Value: 6.8220421318020332664117517756596e+4913
+    //  To chars: 6.8220421318020332664117517756596e+4913
+    //  Snprintf: 6.82204e+4913
+    if ((value > 1e16Q && value < 1e20Q) ||
+        (value > 1e4912Q || value < 1e-4912Q))
+    {
+        return;
+    }
+
+    if( BOOST_TEST_EQ( std::string( buffer, r.ptr ), printf_string ) )
+    {
+    }
+    else
+    {
+        std::cerr << std::setprecision(35)
+                  << "     Value: " << value
+                  << "\n  To chars: " << std::string( buffer, r.ptr )
+                  << "\n  Snprintf: " << printf_string
+                  << "\n    Format: " << error_format << std::endl;
+    }
+}
+
 template <typename T>
 void test_roundtrip_bv()
 {
@@ -127,15 +261,31 @@ int main()
         {
             __float128 w0 = static_cast<__float128>( rng() ); // 0 .. 2^128
             test_roundtrip( w0 );
+            test_sprintf_float( w0, boost::charconv::chars_format::general );
+            test_sprintf_float( w0, boost::charconv::chars_format::scientific );
+            //test_sprintf_float( w0, boost::charconv::chars_format::fixed );
+            //test_sprintf_float( w0, boost::charconv::chars_format::hex );
 
             __float128 w1 = static_cast<__float128>( rng() * q ); // 0.0 .. 1.0
             test_roundtrip( w1 );
+            test_sprintf_float( w1, boost::charconv::chars_format::general );
+            test_sprintf_float( w1, boost::charconv::chars_format::scientific );
+            //test_sprintf_float( w1, boost::charconv::chars_format::fixed );
+            //test_sprintf_float( w1, boost::charconv::chars_format::hex );
 
             __float128 w2 = FLT128_MAX / static_cast<__float128>( rng() ); // large values
             test_roundtrip( w2 );
+            test_sprintf_float( w2, boost::charconv::chars_format::general );
+            test_sprintf_float( w2, boost::charconv::chars_format::scientific );
+            //test_sprintf_float( w2, boost::charconv::chars_format::fixed );
+            //test_sprintf_float( w2, boost::charconv::chars_format::hex );
 
             __float128 w3 = FLT128_MIN * static_cast<__float128>( rng() ); // small values
             test_roundtrip( w3 );
+            test_sprintf_float( w3, boost::charconv::chars_format::general );
+            test_sprintf_float( w3, boost::charconv::chars_format::scientific );
+            //test_sprintf_float( w3, boost::charconv::chars_format::fixed );
+            //test_sprintf_float( w3, boost::charconv::chars_format::hex );
         }
 
         test_roundtrip_bv<__float128>();
