24a1df11a67597d3123abbbeb55e7cfc47417528
Matt Borland <matt@mattborland.com>
2023-06-20 14:50:59 +0200

    Change 128-bit long double bit pattern

--- include/boost/charconv/detail/ryu/ryu_generic_128.hpp
+++ include/boost/charconv/detail/ryu/ryu_generic_128.hpp
@@ -594,7 +594,7 @@ static inline struct floating_decimal_128 long_double_to_fd128(long double d) no
     std::memcpy(&bits, &d, sizeof(long double));
 
     #if LDBL_MANT_DIG == 113 // binary128 (e.g. ARM, S390X)
-    return generic_binary_to_decimal(bits, 113, 15, true);
+    return generic_binary_to_decimal(bits, 112, 15, true);
     #elif LLDBL_MANT_DIG == 106 // ibm128 (e.g. PowerPC)
     return generic_binary_to_decimal(bits, 106, 11, true);
     #endif
