7d18408c0cda83f2607bd7ef430beb77fbca8c64
Matt Borland <matt@mattborland.com>
2023-06-30 16:15:19 +0200

    Fix precision during calculation of z

--- include/boost/charconv/detail/compute_float80.hpp
+++ include/boost/charconv/detail/compute_float80.hpp
@@ -309,7 +309,7 @@ inline ResultType compute_float80(std::int64_t q, Unsigned_Integer w, bool negat
 
     // Step 5a: Compute the truncated 256-bit product stopping after 1 multiplication if
     // no more are required to represetent the number exactly
-    auto z = umul256(significand_256_high[q - smallest_power], w) / (UINT64_MAX);
+    auto z = umul256(significand_256_high[q - smallest_power], w) / uint256{0, UINT64_MAX};
 
     // Step 5b: Some kind of branch to use the second table
 
