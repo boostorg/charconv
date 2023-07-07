65a7264829bc1f5bf019e123efbcc865b3948937
Matt Borland <matt@mattborland.com>
2023-07-07 10:32:09 +0200

    Fix offset for powers implicit in the mantissa

--- include/boost/charconv/detail/compute_float80.hpp
+++ include/boost/charconv/detail/compute_float80.hpp
@@ -286,7 +286,7 @@ inline __float128 compute_float128(std::int64_t q, Unsigned_Integer w, bool nega
 {
     // GLIBC uses 2^-16444 but MPFR uses 2^-16445 as the smallest subnormal value for 80 bit
     // 39 is the max number of digits in an uint128_t
-    static constexpr auto smallest_power = -4951;
+    static constexpr auto smallest_power = -4966; // Includes offset for the powers implicit in the mantissa
     static constexpr auto largest_power = 4932;
 
     #if (FLT_EVAL_METHOD != 1) && (FLT_EVAL_METHOD != 0)
