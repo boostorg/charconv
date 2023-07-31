843b9421f9650ef3ec2ef3c5bc0e5793c8ec4fa0
Matt Borland <matt@mattborland.com>
2023-07-31 16:48:06 -0400

    Remove cruft

--- include/boost/charconv/detail/compute_float80.hpp
+++ include/boost/charconv/detail/compute_float80.hpp
@@ -40,28 +40,6 @@ static constexpr long double powers_of_ten_ld[] = {
     1e49L, 1e50L, 1e51L, 1e52L, 1e53L, 1e54L, 1e55L
 };
 
-static constexpr long double big_powers_of_ten_ld[] = {
-    1e4932L, 1e4931L, 1e4930L, 1e4929L, 1e4928L, 1e4927L,
-    1e4926L, 1e4925L, 1e4924L, 1e4923L, 1e4922L, 1e4921L,
-    1e4920L, 1e4919L, 1e4918L, 1e4917L, 1e4916L, 1e4915L,
-    1e4914L, 1e4913L, 1e4912L, 1e4911L, 1e4910L, 1e4909L,
-    1e4908L, 1e4907L, 1e4906L, 1e4905L, 1e4904L, 1e4903L,
-    1e4902L, 1e4901L, 1e4900L, 1e4899L, 1e4898L, 1e4897L,
-    1e4896L, 1e4895L, 1e4894L, 1e4893L, 1e4892L, 1e4891L,
-    1e4890L, 1e4889L, 1e4888L, 1e4887L, 1e4886L, 1e4885L
-};
-
-static constexpr long double small_powers_of_ten_ld[] = {
-    0.0L, 1e-4950L, 1e-4949L, 1e-4948L, 1e-4947L, 1e-4946L,
-    1e-4945L, 1e-4944L, 1e-4943L, 1e-4942L, 1e-4941L, 1e-4940L,
-    1e-4939L, 1e-4938L, 1e-4937L, 1e-4936L, 1e-4935L, 1e-4934L,
-    1e-4933L, 1e-4932L, 1e-4931L, 1e-4930L, 1e-4929L, 1e-4928L,
-    1e-4927L, 1e-4926L, 1e-4925L, 1e-4924L, 1e-4923L, 1e-4922L,
-    1e-4921L, 1e-4920L, 1e-4919L, 1e-4918L, 1e-4917L, 1e-4916L,
-    1e-4915L, 1e-4914L, 1e-4913L, 1e-4912L, 1e-4911L, 1e-4910L,
-    1e-4909L, 1e-4908L, 1e-4907L, 1e-4906L, 1e-4905L, 1e-4904L
-};
-
 #ifdef BOOST_CHARCONV_HAS_FLOAT128
 static constexpr __float128 powers_of_tenq[] = {
     1e0Q,  1e1Q,  1e2Q,  1e3Q,  1e4Q,  1e5Q,  1e6Q,
@@ -73,36 +51,7 @@ static constexpr __float128 powers_of_tenq[] = {
     1e42Q, 1e43Q, 1e44Q, 1e45Q, 1e46Q, 1e47Q, 1e48Q,
     1e49Q, 1e50Q, 1e51Q, 1e52Q, 1e53Q, 1e54Q, 1e55Q
 };
-
-static constexpr __float128 big_powers_of_ten_q[] = {
-    1e4932Q, 1e4931Q, 1e4930Q, 1e4929Q, 1e4928Q, 1e4927Q,
-    1e4926Q, 1e4925Q, 1e4924Q, 1e4923Q, 1e4922Q, 1e4921Q,
-    1e4920Q, 1e4919Q, 1e4918Q, 1e4917Q, 1e4916Q, 1e4915Q,
-    1e4914Q, 1e4913Q, 1e4912Q, 1e4911Q, 1e4910Q, 1e4909Q,
-    1e4908Q, 1e4907Q, 1e4906Q, 1e4905Q, 1e4904Q, 1e4903Q,
-    1e4902Q, 1e4901Q, 1e4900Q, 1e4899Q, 1e4898Q, 1e4897Q,
-    1e4896Q, 1e4895Q, 1e4894Q, 1e4893Q, 1e4892Q, 1e4891Q,
-    1e4890Q, 1e4889Q, 1e4888Q, 1e4887Q, 1e4886Q, 1e4885Q
-};
-
-static constexpr __float128 small_powers_of_ten_q[] = {
-    0.0Q, 1e-4950Q, 1e-4949Q, 1e-4948Q, 1e-4947Q, 1e-4946Q,
-    1e-4945Q, 1e-4944Q, 1e-4943Q, 1e-4942Q, 1e-4941Q, 1e-4940Q,
-    1e-4939Q, 1e-4938Q, 1e-4937Q, 1e-4936Q, 1e-4935Q, 1e-4934Q,
-    1e-4933Q, 1e-4932Q, 1e-4931Q, 1e-4930Q, 1e-4929Q, 1e-4928Q,
-    1e-4927Q, 1e-4926Q, 1e-4925Q, 1e-4924Q, 1e-4923Q, 1e-4922Q,
-    1e-4921Q, 1e-4920Q, 1e-4919Q, 1e-4918Q, 1e-4917Q, 1e-4916Q,
-    1e-4915Q, 1e-4914Q, 1e-4913Q, 1e-4912Q, 1e-4911Q, 1e-4910Q,
-    1e-4909Q, 1e-4908Q, 1e-4907Q, 1e-4906Q, 1e-4905Q, 1e-4904Q
-};
 #endif
-
-// Notation:
-// m -> binary significand
-// p -> binary exponent
-//
-// w -> decimal significand
-// q -> decimal exponent
 
 template <typename ResultType, typename Unsigned_Integer, typename ArrayPtr>
 inline long double fast_path(std::int64_t q, Unsigned_Integer w, bool negative, ArrayPtr table) noexcept
