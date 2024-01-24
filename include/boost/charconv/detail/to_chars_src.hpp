26a64e277ddbdad1293b70c292b77acea8acccae
Matt Borland <matt@mattborland.com>
2024-01-24 10:12:35 +0100

    Fix formatting issues

--- include/boost/charconv/detail/to_chars_src.hpp
+++ include/boost/charconv/detail/to_chars_src.hpp
@@ -269,262 +269,262 @@ BOOST_FORCEINLINE static void print_9_digits(std::uint32_t s32, int& exponent,
 template <>
 char* to_chars<float, dragonbox_float_traits<float>>(std::uint32_t s32, int exponent, char* buffer, chars_format fmt) noexcept
 {
-// Print significand.
-print_9_digits(s32, exponent, buffer);
+    // Print significand.
+    print_9_digits(s32, exponent, buffer);
 
-// Print exponent and return
-if (exponent < 0)
-{
-std::memcpy(buffer, "e-", 2);
-buffer += 2;
-exponent = -exponent;
-}
-else if (exponent == 0)
-{
-if (fmt == chars_format::scientific)
-{
-std::memcpy(buffer, "e+00", 4);
-buffer += 4;
-}
+    // Print exponent and return
+    if (exponent < 0)
+    {
+        std::memcpy(buffer, "e-", 2);
+        buffer += 2;
+        exponent = -exponent;
+    }
+    else if (exponent == 0)
+    {
+        if (fmt == chars_format::scientific)
+        {
+            std::memcpy(buffer, "e+00", 4);
+            buffer += 4;
+        }
 
-return buffer;
-}
-else
-{
-std::memcpy(buffer, "e+", 2);
-buffer += 2;
-}
+        return buffer;
+    }
+    else
+    {
+        std::memcpy(buffer, "e+", 2);
+        buffer += 2;
+    }
 
-print_2_digits(std::uint32_t(exponent), buffer);
-buffer += 2;
+    print_2_digits(std::uint32_t(exponent), buffer);
+    buffer += 2;
 
-return buffer;
+    return buffer;
 }
 
 template <>
 char* to_chars<double, dragonbox_float_traits<double>>(const std::uint64_t significand, int exponent, char* buffer, chars_format fmt) noexcept {
-// Print significand by decomposing it into a 9-digit block and a 8-digit block.
-std::uint32_t first_block;
-std::uint32_t second_block {};
-bool no_second_block;
+    // Print significand by decomposing it into a 9-digit block and a 8-digit block.
+    std::uint32_t first_block;
+    std::uint32_t second_block {};
+    bool no_second_block;
 
-if (significand >= 100000000)
-{
-first_block = std::uint32_t(significand / 100000000);
-second_block = std::uint32_t(significand) - first_block * 100000000;
-exponent += 8;
-no_second_block = (second_block == 0);
-}
-else
-{
-first_block = std::uint32_t(significand);
-no_second_block = true;
-}
+    if (significand >= 100000000)
+    {
+        first_block = std::uint32_t(significand / 100000000);
+        second_block = std::uint32_t(significand) - first_block * 100000000;
+        exponent += 8;
+        no_second_block = (second_block == 0);
+    }
+    else
+    {
+        first_block = std::uint32_t(significand);
+        no_second_block = true;
+    }
 
-if (no_second_block)
-{
-print_9_digits(first_block, exponent, buffer);
-}
-else
-{
-// We proceed similarly to print_9_digits(), but since we do not need to remove
-// trailing zeros, the procedure is a bit simpler.
-if (first_block >= 100000000)
-{
-// The input is of 17 digits, thus there should be no trailing zero at all.
-// The first block is of 9 digits.
-// 1441151882 = ceil(2^57 / 1'0000'0000) + 1
-auto prod = first_block * std::uint64_t(1441151882);
-prod >>= 25;
-std::memcpy(buffer, radix_100_head_table + std::uint32_t(prod >> 32) * 2, 2);
-prod = std::uint32_t(prod) * std::uint64_t(100);
-print_2_digits(std::uint32_t(prod >> 32), buffer + 2);
-prod = std::uint32_t(prod) * std::uint64_t(100);
-print_2_digits(std::uint32_t(prod >> 32), buffer + 4);
-prod = std::uint32_t(prod) * std::uint64_t(100);
-print_2_digits(std::uint32_t(prod >> 32), buffer + 6);
-prod = std::uint32_t(prod) * std::uint64_t(100);
-print_2_digits(std::uint32_t(prod >> 32), buffer + 8);
+    if (no_second_block)
+    {
+        print_9_digits(first_block, exponent, buffer);
+    }
+    else
+    {
+        // We proceed similarly to print_9_digits(), but since we do not need to remove
+        // trailing zeros, the procedure is a bit simpler.
+        if (first_block >= 100000000)
+        {
+            // The input is of 17 digits, thus there should be no trailing zero at all.
+            // The first block is of 9 digits.
+            // 1441151882 = ceil(2^57 / 1'0000'0000) + 1
+            auto prod = first_block * std::uint64_t(1441151882);
+            prod >>= 25;
+            std::memcpy(buffer, radix_100_head_table + std::uint32_t(prod >> 32) * 2, 2);
+            prod = std::uint32_t(prod) * std::uint64_t(100);
+            print_2_digits(std::uint32_t(prod >> 32), buffer + 2);
+            prod = std::uint32_t(prod) * std::uint64_t(100);
+            print_2_digits(std::uint32_t(prod >> 32), buffer + 4);
+            prod = std::uint32_t(prod) * std::uint64_t(100);
+            print_2_digits(std::uint32_t(prod >> 32), buffer + 6);
+            prod = std::uint32_t(prod) * std::uint64_t(100);
+            print_2_digits(std::uint32_t(prod >> 32), buffer + 8);
 
-// The second block is of 8 digits.
-// 281474978 = ceil(2^48 / 100'0000) + 1
-prod = second_block * std::uint64_t(281474978);
-prod >>= 16;
-prod += 1;
-print_2_digits(std::uint32_t(prod >> 32), buffer + 10);
-prod = std::uint32_t(prod) * std::uint64_t(100);
-print_2_digits(std::uint32_t(prod >> 32), buffer + 12);
-prod = std::uint32_t(prod) * std::uint64_t(100);
-print_2_digits(std::uint32_t(prod >> 32), buffer + 14);
-prod = std::uint32_t(prod) * std::uint64_t(100);
-print_2_digits(std::uint32_t(prod >> 32), buffer + 16);
+            // The second block is of 8 digits.
+            // 281474978 = ceil(2^48 / 100'0000) + 1
+            prod = second_block * std::uint64_t(281474978);
+            prod >>= 16;
+            prod += 1;
+            print_2_digits(std::uint32_t(prod >> 32), buffer + 10);
+            prod = std::uint32_t(prod) * std::uint64_t(100);
+            print_2_digits(std::uint32_t(prod >> 32), buffer + 12);
+            prod = std::uint32_t(prod) * std::uint64_t(100);
+            print_2_digits(std::uint32_t(prod >> 32), buffer + 14);
+            prod = std::uint32_t(prod) * std::uint64_t(100);
+            print_2_digits(std::uint32_t(prod >> 32), buffer + 16);
 
-exponent += 8;
-buffer += 18;
-}
-else
-{
-if (first_block >= 1000000)
-{
-// 7 or 8 digits.
-// 281474978 = ceil(2^48 / 100'0000) + 1
-auto prod = first_block * std::uint64_t(281474978);
-prod >>= 16;
-const auto head_digits = std::uint32_t(prod >> 32);
+            exponent += 8;
+            buffer += 18;
+        }
+        else
+        {
+            if (first_block >= 1000000)
+            {
+            // 7 or 8 digits.
+            // 281474978 = ceil(2^48 / 100'0000) + 1
+            auto prod = first_block * std::uint64_t(281474978);
+            prod >>= 16;
+            const auto head_digits = std::uint32_t(prod >> 32);
 
-std::memcpy(buffer, radix_100_head_table + head_digits * 2, 2);
-buffer[2] = radix_table[head_digits * 2 + 1];
+            std::memcpy(buffer, radix_100_head_table + head_digits * 2, 2);
+            buffer[2] = radix_table[head_digits * 2 + 1];
 
-exponent += static_cast<int>(6 + unsigned(head_digits >= 10));
-buffer += unsigned(head_digits >= 10);
+            exponent += static_cast<int>(6 + unsigned(head_digits >= 10));
+            buffer += unsigned(head_digits >= 10);
 
-// Print remaining 6 digits.
-prod = std::uint32_t(prod) * std::uint64_t(100);
-print_2_digits(std::uint32_t(prod >> 32), buffer + 2);
-prod = std::uint32_t(prod) * std::uint64_t(100);
-print_2_digits(std::uint32_t(prod >> 32), buffer + 4);
-prod = std::uint32_t(prod) * std::uint64_t(100);
-print_2_digits(std::uint32_t(prod >> 32), buffer + 6);
+            // Print remaining 6 digits.
+            prod = std::uint32_t(prod) * std::uint64_t(100);
+            print_2_digits(std::uint32_t(prod >> 32), buffer + 2);
+            prod = std::uint32_t(prod) * std::uint64_t(100);
+            print_2_digits(std::uint32_t(prod >> 32), buffer + 4);
+            prod = std::uint32_t(prod) * std::uint64_t(100);
+            print_2_digits(std::uint32_t(prod >> 32), buffer + 6);
 
-buffer += 8;
-}
-else if (first_block >= 10000)
-{
-// 5 or 6 digits.
-// 429497 = ceil(2^32 / 1'0000)
-auto prod = first_block * std::uint64_t(429497);
-const auto head_digits = std::uint32_t(prod >> 32);
+            buffer += 8;
+            }
+            else if (first_block >= 10000)
+            {
+                // 5 or 6 digits.
+                // 429497 = ceil(2^32 / 1'0000)
+                auto prod = first_block * std::uint64_t(429497);
+                const auto head_digits = std::uint32_t(prod >> 32);
 
-std::memcpy(buffer, radix_100_head_table + head_digits * 2, 2);
-buffer[2] = radix_table[head_digits * 2 + 1];
+                std::memcpy(buffer, radix_100_head_table + head_digits * 2, 2);
+                buffer[2] = radix_table[head_digits * 2 + 1];
 
-exponent += static_cast<int>(4 + unsigned(head_digits >= 10));
-buffer += unsigned(head_digits >= 10);
+                exponent += static_cast<int>(4 + unsigned(head_digits >= 10));
+                buffer += unsigned(head_digits >= 10);
 
-// Print remaining 4 digits.
-prod = std::uint32_t(prod) * std::uint64_t(100);
-print_2_digits(std::uint32_t(prod >> 32), buffer + 2);
-prod = std::uint32_t(prod) * std::uint64_t(100);
-print_2_digits(std::uint32_t(prod >> 32), buffer + 4);
+                // Print remaining 4 digits.
+                prod = std::uint32_t(prod) * std::uint64_t(100);
+                print_2_digits(std::uint32_t(prod >> 32), buffer + 2);
+                prod = std::uint32_t(prod) * std::uint64_t(100);
+                print_2_digits(std::uint32_t(prod >> 32), buffer + 4);
 
-buffer += 6;
-}
-else if (first_block >= 100)
-{
-// 3 or 4 digits.
-// 42949673 = ceil(2^32 / 100)
-auto prod = first_block * std::uint64_t(42949673);
-const auto head_digits = std::uint32_t(prod >> 32);
+                buffer += 6;
+            }
+            else if (first_block >= 100)
+            {
+                // 3 or 4 digits.
+                // 42949673 = ceil(2^32 / 100)
+                auto prod = first_block * std::uint64_t(42949673);
+                const auto head_digits = std::uint32_t(prod >> 32);
 
-std::memcpy(buffer, radix_100_head_table + head_digits * 2, 2);
-buffer[2] = radix_table[head_digits * 2 + 1];
+                std::memcpy(buffer, radix_100_head_table + head_digits * 2, 2);
+                buffer[2] = radix_table[head_digits * 2 + 1];
 
-exponent += static_cast<int>(2 + unsigned(head_digits >= 10));
-buffer += unsigned(head_digits >= 10);
+                exponent += static_cast<int>(2 + unsigned(head_digits >= 10));
+                buffer += unsigned(head_digits >= 10);
 
-// Print remaining 2 digits.
-prod = std::uint32_t(prod) * std::uint64_t(100);
-print_2_digits(std::uint32_t(prod >> 32), buffer + 2);
+                // Print remaining 2 digits.
+                prod = std::uint32_t(prod) * std::uint64_t(100);
+                print_2_digits(std::uint32_t(prod >> 32), buffer + 2);
 
-buffer += 4;
-}
-else
-{
-// 1 or 2 digits.
-std::memcpy(buffer, radix_100_head_table + first_block * 2, 2);
-buffer[2] = radix_table[first_block * 2 + 1];
+                buffer += 4;
+            }
+            else
+            {
+                // 1 or 2 digits.
+                std::memcpy(buffer, radix_100_head_table + first_block * 2, 2);
+                buffer[2] = radix_table[first_block * 2 + 1];
 
-exponent += (first_block >= 10);
-buffer += (2 + unsigned(first_block >= 10));
-}
+                exponent += (first_block >= 10);
+                buffer += (2 + unsigned(first_block >= 10));
+            }
 
-// Next, print the second block.
-// The second block is of 8 digits, but we may have trailing zeros.
-// 281474978 = ceil(2^48 / 100'0000) + 1
-auto prod = second_block * std::uint64_t(281474978);
-prod >>= 16;
-prod += 1;
-print_2_digits(std::uint32_t(prod >> 32), buffer);
+            // Next, print the second block.
+            // The second block is of 8 digits, but we may have trailing zeros.
+            // 281474978 = ceil(2^48 / 100'0000) + 1
+            auto prod = second_block * std::uint64_t(281474978);
+            prod >>= 16;
+            prod += 1;
+            print_2_digits(std::uint32_t(prod >> 32), buffer);
 
-// Remaining 6 digits are all zero?
-if (std::uint32_t(prod) <= std::uint32_t((std::uint64_t(1) << 32) / 1000000))
-{
-buffer += (1 + unsigned(buffer[1] > '0'));
-}
-else
-{
-// Obtain the next two digits.
-prod = std::uint32_t(prod) * std::uint64_t(100);
-print_2_digits(std::uint32_t(prod >> 32), buffer + 2);
+            // Remaining 6 digits are all zero?
+            if (std::uint32_t(prod) <= std::uint32_t((std::uint64_t(1) << 32) / 1000000))
+            {
+                buffer += (1 + unsigned(buffer[1] > '0'));
+            }
+            else
+            {
+                // Obtain the next two digits.
+                prod = std::uint32_t(prod) * std::uint64_t(100);
+                print_2_digits(std::uint32_t(prod >> 32), buffer + 2);
 
-// Remaining 4 digits are all zero?
-if (std::uint32_t(prod) <= std::uint32_t((std::uint64_t(1) << 32) / 10000))
-{
-buffer += (3 + unsigned(buffer[3] > '0'));
-}
-else
-{
-// Obtain the next two digits.
-prod = std::uint32_t(prod) * std::uint64_t(100);
-print_2_digits(std::uint32_t(prod >> 32), buffer + 4);
+                // Remaining 4 digits are all zero?
+                if (std::uint32_t(prod) <= std::uint32_t((std::uint64_t(1) << 32) / 10000))
+                {
+                    buffer += (3 + unsigned(buffer[3] > '0'));
+                }
+                else
+                {
+                    // Obtain the next two digits.
+                    prod = std::uint32_t(prod) * std::uint64_t(100);
+                    print_2_digits(std::uint32_t(prod >> 32), buffer + 4);
 
-// Remaining 2 digits are all zero?
-if (std::uint32_t(prod) <= std::uint32_t((std::uint64_t(1) << 32) / 100))
-{
-buffer += (5 + unsigned(buffer[5] > '0'));
-}
-else
-{
-// Obtain the last two digits.
-prod = std::uint32_t(prod) * std::uint64_t(100);
-print_2_digits(std::uint32_t(prod >> 32), buffer + 6);
-buffer += (7 + unsigned(buffer[7] > '0'));
-}
-}
-}
-}
-}
-if (exponent < 0)
-{
-std::memcpy(buffer, "e-", 2);
-buffer += 2;
-exponent = -exponent;
-}
-else if (exponent == 0)
-{
-if (fmt == chars_format::scientific)
-{
-std::memcpy(buffer, "e+00", 4);
-buffer += 4;
-}
+                    // Remaining 2 digits are all zero?
+                    if (std::uint32_t(prod) <= std::uint32_t((std::uint64_t(1) << 32) / 100))
+                    {
+                        buffer += (5 + unsigned(buffer[5] > '0'));
+                    }
+                    else
+                    {
+                        // Obtain the last two digits.
+                        prod = std::uint32_t(prod) * std::uint64_t(100);
+                        print_2_digits(std::uint32_t(prod >> 32), buffer + 6);
+                        buffer += (7 + unsigned(buffer[7] > '0'));
+                    }
+                }
+            }
+        }
+    }
+    if (exponent < 0)
+    {
+        std::memcpy(buffer, "e-", 2);
+        buffer += 2;
+        exponent = -exponent;
+    }
+    else if (exponent == 0)
+    {
+        if (fmt == chars_format::scientific)
+        {
+            std::memcpy(buffer, "e+00", 4);
+            buffer += 4;
+        }
 
-return buffer;
-}
-else
-{
-std::memcpy(buffer, "e+", 2);
-buffer += 2;
-}
+        return buffer;
+    }
+    else
+    {
+        std::memcpy(buffer, "e+", 2);
+        buffer += 2;
+    }
 
-if (exponent >= 100)
-{
-// d1 = exponent / 10; d2 = exponent % 10;
-// 6554 = ceil(2^16 / 10)
-auto prod = std::uint32_t(exponent) * std::uint32_t(6554);
-auto d1 = prod >> 16;
-prod = std::uint16_t(prod) * std::uint32_t(5); // * 10
-auto d2 = prod >> 15;                          // >> 16
-print_2_digits(d1, buffer);
-print_1_digit(d2, buffer + 2);
-buffer += 3;
-}
-else
-{
-print_2_digits(static_cast<std::uint32_t>(exponent), buffer);
-buffer += 2;
-}
+    if (exponent >= 100)
+    {
+        // d1 = exponent / 10; d2 = exponent % 10;
+        // 6554 = ceil(2^16 / 10)
+        auto prod = std::uint32_t(exponent) * std::uint32_t(6554);
+        auto d1 = prod >> 16;
+        prod = std::uint16_t(prod) * std::uint32_t(5); // * 10
+        auto d2 = prod >> 15;                          // >> 16
+        print_2_digits(d1, buffer);
+        print_1_digit(d2, buffer + 2);
+        buffer += 3;
+    }
+    else
+    {
+        print_2_digits(static_cast<std::uint32_t>(exponent), buffer);
+        buffer += 2;
+    }
 
-return buffer;
+    return buffer;
 }
 
 #ifdef BOOST_MSVC
