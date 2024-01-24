4f5ba9eeef6f2b9a9463d6c47e8dd4d449e065e1
Matt Borland <matt@mattborland.com>
2024-01-24 09:46:12 +0100

    Refactor into header versions of src files

---
+++ include/boost/charconv/src.hpp
@@ -0,0 +1,13 @@
+// Copyright 2024 Matt Borland
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+//
+// To use this library in header only mode include this file in exactly ONE file
+
+#ifndef BOOST_CHARCONV_SRC_HPP
+#define BOOST_CHARCONV_SRC_HPP
+
+#include <boost/charconv/detail/from_chars_src.hpp>
+#include <boost/charconv/detail/to_chars_src.hpp>
+
+#endif //BOOST_CHARCONV_SRC_HPP
