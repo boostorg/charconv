b9928e5a6083ec4b9fe31faa6018a29c88152705
Matt Borland <matt@mattborland.com>
2024-02-09 12:09:33 +0100

    Add additional testing for positive exps

--- test/github_issue_158.cpp
+++ test/github_issue_158.cpp
@@ -1,4 +1,5 @@
 // Copyright 2024 Matt Borland
+// Copyright 2024 Junekey Jeon
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 //
@@ -7,9 +8,9 @@
 #include <boost/charconv.hpp>
 #include <boost/core/lightweight_test.hpp>
 
-int main()
+void test_values_with_negative_exp()
 {
-    char buffer[60];
+    char buffer[256];
     double d = 1e-15;
     auto res = boost::charconv::to_chars(buffer, buffer + sizeof(buffer), d,
                          boost::charconv::chars_format::scientific, 50);
@@ -37,6 +38,44 @@ int main()
     *res.ptr = '\0';
     BOOST_TEST(res);
     BOOST_TEST_CSTR_EQ(buffer, "0.00000000000000001000000000000000071542424054621925");
+}
+
+void test_values_with_positive_exp()
+{
+    char buffer[256];
+    double d = 1e15;
+    auto res = boost::charconv::to_chars(buffer, buffer + sizeof(buffer), d,
+                         boost::charconv::chars_format::scientific, 50);
+    *res.ptr = '\0';
+
+    BOOST_TEST(res);
+    BOOST_TEST_CSTR_EQ(buffer, "1.00000000000000000000000000000000000000000000000000e+15");
+
+    res = boost::charconv::to_chars(buffer, buffer + sizeof(buffer), d,
+                         boost::charconv::chars_format::fixed, 50);
+    *res.ptr = '\0';
+    BOOST_TEST(res);
+    BOOST_TEST_CSTR_EQ(buffer, "1000000000000000.00000000000000000000000000000000000000000000000000");
+
+    d = 1e17;
+
+    res = boost::charconv::to_chars(buffer, buffer + sizeof(buffer), d,
+                         boost::charconv::chars_format::scientific, 50);
+    *res.ptr = '\0';
+    BOOST_TEST(res);
+    BOOST_TEST_CSTR_EQ(buffer, "1.00000000000000000000000000000000000000000000000000e+17");
+
+    res = boost::charconv::to_chars(buffer, buffer + sizeof(buffer), d,
+                         boost::charconv::chars_format::fixed, 50);
+    *res.ptr = '\0';
+    BOOST_TEST(res);
+    BOOST_TEST_CSTR_EQ(buffer, "100000000000000000.00000000000000000000000000000000000000000000000000");
+}
+
+int main()
+{
+    //test_values_with_negative_exp();
+    test_values_with_positive_exp();
 
     return boost::report_errors();
 }
