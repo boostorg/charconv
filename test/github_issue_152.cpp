a81a0afe62225c71de65abe9cde21d49b8f1e884
Matt Borland <matt@mattborland.com>
2024-02-12 09:36:56 +0100

    Add fixed precision buffer overflow tests

--- test/github_issue_152.cpp
+++ test/github_issue_152.cpp
@@ -51,6 +51,50 @@ void test_non_finite()
     auto r_snan = boost::charconv::to_chars(snan_buffer, snan_buffer + 9, snan_val);
     BOOST_TEST(r_snan);
     BOOST_TEST(!std::memcmp(snan_buffer, "nan(snan)", 9));
+}
+
+template <typename T>
+void test_non_finite_fixed_precision()
+{
+    constexpr std::array<T, 6> values = {{std::numeric_limits<T>::infinity(), -std::numeric_limits<T>::infinity(),
+                                         std::numeric_limits<T>::quiet_NaN(), -std::numeric_limits<T>::quiet_NaN(),
+                                         std::numeric_limits<T>::signaling_NaN(), -std::numeric_limits<T>::signaling_NaN()}};
+
+    const auto formats = {boost::charconv::chars_format::scientific, boost::charconv::chars_format::hex,
+                              boost::charconv::chars_format::general, boost::charconv::chars_format::fixed};
+
+    for (auto format : formats)
+    {
+        for (const auto val : values)
+        {
+            char buffer[2];
+            auto r = boost::charconv::to_chars(buffer, buffer + sizeof(buffer), val, format, 5);
+            BOOST_TEST(r.ec == std::errc::result_out_of_range);
+        }
+
+        char inf_buffer[3];
+        constexpr T inf_val = std::numeric_limits<T>::infinity();
+        auto r_inf = boost::charconv::to_chars(inf_buffer, inf_buffer + 3, inf_val, format, 5);
+        BOOST_TEST(r_inf);
+        BOOST_TEST(!std::memcmp(inf_buffer, "inf", 3));
+
+        char nan_buffer[3];
+        constexpr T nan_val = std::numeric_limits<T>::quiet_NaN();
+        auto r_nan = boost::charconv::to_chars(nan_buffer, nan_buffer + 3, nan_val, format, 5);
+        BOOST_TEST(r_nan);
+        BOOST_TEST(!std::memcmp(nan_buffer, "nan", 3));
+
+        char neg_nan_buffer[9];
+        auto r_neg_nan = boost::charconv::to_chars(neg_nan_buffer, neg_nan_buffer + 9, -nan_val, format, 5);
+        BOOST_TEST(r_neg_nan);
+        BOOST_TEST(!std::memcmp(neg_nan_buffer, "-nan(ind)", 9));
+
+        char snan_buffer[9];
+        constexpr T snan_val = std::numeric_limits<T>::signaling_NaN();
+        auto r_snan = boost::charconv::to_chars(snan_buffer, snan_buffer + 9, snan_val, format, 5);
+        BOOST_TEST(r_snan);
+        BOOST_TEST(!std::memcmp(snan_buffer, "nan(snan)", 9));
+    }
 };
 
 #ifdef BOOST_MSVC
@@ -151,10 +195,44 @@ int main()
     test_non_finite<float>();
     test_non_finite<double>();
     test_non_finite<long double>();
+    #ifdef BOOST_CHARCONV_HAS_FLOAT16
+    test_non_finite<std::float16_t>();
+    #endif
+    #ifdef BOOST_CHARCONV_HAS_FLOAT32
+    test_non_finite<std::float32_t>();
+    #endif
+    #ifdef BOOST_CHARCONV_HAS_FLOAT64
+    test_non_finite<std::float64_t>();
+    #endif
+    #ifdef BOOST_CHARCONV_HAS_BRAINFLOAT16
+    test_non_finite<std::bfloat16_t>();
+    #endif
+
+    test_non_finite_fixed_precision<float>();
+    test_non_finite_fixed_precision<double>();
+    test_non_finite_fixed_precision<long double>();
+    #ifdef BOOST_CHARCONV_HAS_FLOAT16
+    test_non_finite_fixed_precision<std::float16_t>();
+    #endif
+    #ifdef BOOST_CHARCONV_HAS_FLOAT32
+    test_non_finite_fixed_precision<std::float32_t>();
+    #endif
+    #ifdef BOOST_CHARCONV_HAS_FLOAT64
+    test_non_finite_fixed_precision<std::float64_t>();
+    #endif
+    #ifdef BOOST_CHARCONV_HAS_BRAINFLOAT16
+    test_non_finite_fixed_precision<std::bfloat16_t>();
+    #endif
 
     test_min_buffer_size<float>();
     test_min_buffer_size<double>();
     test_min_buffer_size<long double>();
+    #ifdef BOOST_CHARCONV_HAS_FLOAT32
+    test_min_buffer_size<std::float32_t>();
+    #endif
+    #ifdef BOOST_CHARCONV_HAS_FLOAT64
+    test_min_buffer_size<std::float64_t>();
+    #endif
 
     #if BOOST_CHARCONV_LDBL_BITS > 64
     test_failed_values();
