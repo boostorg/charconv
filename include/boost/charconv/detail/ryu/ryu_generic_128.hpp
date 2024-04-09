46ad35afcaadb565a79be4d2c428e18559a76335
Matt Borland <matt@mattborland.com>
2024-04-09 09:25:38 +0200

    Move ryu functions

--- include/boost/charconv/detail/ryu/ryu_generic_128.hpp
+++ include/boost/charconv/detail/ryu/ryu_generic_128.hpp
@@ -682,42 +682,6 @@ static inline struct floating_decimal_128 long_double_to_fd128(long double d) no
 
 #endif
 
-#ifdef BOOST_CHARCONV_HAS_FLOAT128
-
-static inline struct floating_decimal_128 float128_to_fd128(__float128 d) noexcept
-{
-    #ifdef BOOST_CHARCONV_HAS_INT128
-    unsigned_128_type bits = 0;
-    std::memcpy(&bits, &d, sizeof(__float128));
-    #else
-    trivial_uint128 trivial_bits;
-    std::memcpy(&trivial_bits, &d, sizeof(__float128));
-    unsigned_128_type bits {trivial_bits};
-    #endif
-
-    return generic_binary_to_decimal(bits, 112, 15, false);
-}
-
-#endif
-
-#ifdef BOOST_CHARCONV_HAS_STDFLOAT128
-
-static inline struct floating_decimal_128 stdfloat128_to_fd128(std::float128_t d) noexcept
-{
-    #ifdef BOOST_CHARCONV_HAS_INT128
-    unsigned_128_type bits = 0;
-    std::memcpy(&bits, &d, sizeof(std::float128_t));
-    #else
-    trivial_uint128 trivial_bits;
-    std::memcpy(&trivial_bits, &d, sizeof(std::float128_t));
-    unsigned_128_type bits {trivial_bits};
-    #endif
-
-    return generic_binary_to_decimal(bits, 112, 15, false);
-}
-
-#endif
-
 }}}} // Namespaces
 
 #endif //BOOST_RYU_GENERIC_128_HPP
