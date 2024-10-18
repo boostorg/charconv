8e3179ea286edc4d4334da17f5952d5d1af4ca93
Matt Borland <matt@mattborland.com>
2024-10-18 10:52:34 -0400

    Add test set

---
+++ test/github_issue_166_float128.cpp
@@ -0,0 +1,42 @@
+// Copyright 2024 Matt Borland
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+//
+// See: https://github.com/boostorg/charconv/issues/166
+
+#include <boost/charconv.hpp>
+
+#ifdef BOOST_CHARCONV_HAS_STDFLOAT128
+
+#include <boost/core/lightweight_test.hpp>
+#include <stdfloat>
+#include <string>
+
+template <typename T>
+void test()
+{
+    constexpr T value = 3746.348756384763;
+    constexpr int precision = 6;
+
+    char buffer[1024];
+    const auto result = boost::charconv::to_chars(buffer, buffer + sizeof(buffer), value, boost::charconv::chars_format::fixed, precision);
+    BOOST_TEST(result.ec == std::errc());
+    BOOST_TEST_EQ(std::string{buffer}, std::to_string(3746.348756));
+}
+
+int main()
+{
+    test<__float128>();
+    test<std::float128_t>();
+
+    return boost::report_errors();
+}
+
+#else
+
+int main()
+{
+    return 0;
+}
+
+#endif
