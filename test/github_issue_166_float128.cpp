7218d59ddfb81211f2b11f916f3c9153b69cc6cc
Matt Borland <matt@mattborland.com>
2024-10-28 09:40:07 -0400

    Add further rounding test analogous to Gero's test set

--- test/github_issue_166_float128.cpp
+++ test/github_issue_166_float128.cpp
@@ -48,6 +48,39 @@ void more_rounding()
 }
 
 template <typename T>
+void further_rounding()
+{
+    const std::array<std::string, 8U> solutions {
+        "3331.5",
+        "3331.52",
+        "3331.520",
+        "3331.5199",
+        "3331.51989",
+        "3331.519894",
+        "3331.5198945",
+        "3331.51989448"
+    };
+
+    constexpr T value = 3331.519894481067353L;
+
+    for (int i = 1; i < 9; ++i)
+    {
+        char buffer[1024];
+        const auto result = boost::charconv::to_chars(buffer, buffer + sizeof(buffer), value, boost::charconv::chars_format::fixed, i);
+        *result.ptr = '\0';
+        BOOST_TEST(result.ec == std::errc());
+        if (!BOOST_TEST_EQ(std::string(buffer), solutions[static_cast<std::size_t>(i - 1)]))
+        {
+            // LCOV_EXCL_START
+            std::cerr << "Precision: " << i
+                      << "\nExpected: " << solutions[static_cast<std::size_t>(i - 1)]
+                      << "\n     Got: " << buffer << std::endl;
+            // LCOV_EXCL_STOP
+        }
+    }
+}
+
+template <typename T>
 void full_rounding_test()
 {
     constexpr T value = 9999.999999999999999999L;
@@ -79,6 +112,7 @@ int main()
     test<long double>();
     rounding<long double>();
     more_rounding<long double>();
+    further_rounding<long double>();
     full_rounding_test<long double>();
     test_zeros_path<long double>();
     #endif
@@ -87,6 +121,7 @@ int main()
     test<__float128>();
     rounding<__float128>();
     more_rounding<__float128>();
+    further_rounding<__float128>();
     full_rounding_test<__float128>();
     test_zeros_path<__float128>();
     #endif
@@ -95,6 +130,7 @@ int main()
     test<std::float128_t>();
     rounding<std::float128_t>();
     more_rounding<std::float128_t>();
+    further_rounding<std::float128_t>();
     full_rounding_test<std::float128_t>();
     test_zeros_path<std::float128_t>();
     #endif
