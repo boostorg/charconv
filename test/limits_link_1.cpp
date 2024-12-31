454289130ee857d18020b962c194cd9e794aa64d
Ruben Perez <rubenperez038@gmail.com>
2024-12-31 15:31:18 +0100

    Test: limits_link_1

--- test/limits_link_1.cpp
+++ test/limits_link_1.cpp
@@ -2,8 +2,14 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
+#include <boost/config.hpp>
+#include <boost/charconv/detail/config.hpp>
+#ifdef BOOST_USE_MODULES
+import boost.charconv;
+#else
 #include <boost/charconv/limits.hpp>
-#include <boost/charconv/detail/bit_layouts.hpp>
+#endif
+
 
 void test_odr_use( int const* );
 
