fd5101ba6ab4d823dae8224e34ac3b8f990cbae0
Ruben Perez <rubenperez038@gmail.com>
2024-12-31 19:23:19 +0100

    Test: issue 154

--- test/github_issue_154.cpp
+++ test/github_issue_154.cpp
@@ -4,8 +4,17 @@
 //
 // See: https://github.com/boostorg/charconv/issues/154
 
+#ifdef BOOST_USE_MODULES
+import std;
+import boost.core;
+import boost.charconv;
+#include <boost/core/lightweight_test_macros.hpp>
+#else
 #include <boost/charconv.hpp>
 #include <boost/core/lightweight_test.hpp>
+#endif
+
+#include <boost/charconv/detail/config.hpp>
 
 int main()
 {
