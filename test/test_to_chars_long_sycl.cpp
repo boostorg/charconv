ced70da112458b70e3cf95601d97a07d09c775b7
Matt Borland <matt@mattborland.com>
2026-07-09 09:33:41 -0400

    Add SYCL tests

---
+++ test/test_to_chars_long_sycl.cpp
@@ -0,0 +1,10 @@
+//  Copyright 2026 Matt Borland
+//  Distributed under the Boost Software License, Version 1.0.
+//  https://www.boost.org/LICENSE_1_0.txt
+
+#include "sycl_charconv_test.hpp"
+
+int main()
+{
+    return charconv_sycl_test::test_to_chars<long>();
+}
